52573ec2617fc3a9baa8338a5eeb5c9513bd1fc4
Enesmerdane <ensmerdane@gmail.com>
2019-10-03 11:01:48 +0300

    A little change

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@
 
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello Game!");
+        System.out.println("Hello!");
+        System.out.println("Welcome to our game");
     }
 }
