978887538081e8bfb3d78929369d4b67ad452e52
Enesmerdane <ensmerdane@gmail.com>
2019-11-13 10:53:48 +0300

    Buttons and Background image setting operations are moved to the functions defined below
    Buttons and Background image setting operations are moved to the functions defined below

--- src/SettlersOfAnatolia/src/main/java/com/groupb/soa/MainApp.java
+++ src/SettlersOfAnatolia/src/main/java/com/groupb/soa/MainApp.java
@@ -1,28 +1,58 @@
 package com.groupb.soa;
 
-import java.awt.Rectangle;
-import java.io.FileInputStream;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.ImagePattern;
 import javafx.stage.Stage;
-
 
 public class MainApp extends Application {
     
     private ImageView background_image_view;
     private Image background_image;
+    private Button continue_game_button;
+    
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainMenuScene.fxml"));
         
+        setButtons();
+        
+        Group group = new Group();
+        
+        group.getChildren().add(continue_game_button);
+        
+        Scene scene = new Scene(group, 500, 400);
+        
+        scene.getStylesheets().add("/styles/Styles.css");
+        
+        setBackgroundImage(scene);
+        
+        stage.setTitle("JavaFX and Maven");
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.show();
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    private void setBackgroundImage(Scene scene){
         try {
             background_image = new Image("/images/main_menu_scene_background.jpg");
             background_image_view.setImage(background_image);
@@ -42,28 +72,19 @@ public class MainApp extends Application {
             System.out.println("File cannot be loaded.");
         }
         
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css");
-        
         ImagePattern pattern = new ImagePattern(background_image);
         scene.setFill(pattern);
         
-        stage.setTitle("JavaFX and Maven");
-        stage.setScene(scene);
-        stage.setFullScreen(true);
-        stage.show();
     }
+    
+    private void setButtons(){
+        continue_game_button = new Button("Continue Game");
+        
+        continue_game_button.setStyle("-fx-border-radius: 30;");
+        continue_game_button.setScaleX(2);
+        continue_game_button.setScaleY(2);
+        continue_game_button.setLayoutX(300);
+        continue_game_button.setLayoutY(400);
 
-    /**
-     * The main() method is ignored in correctly deployed JavaFX application.
-     * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts, e.g., in IDEs with limited FX
-     * support. NetBeans ignores main().
-     *
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
     }
-
 }
