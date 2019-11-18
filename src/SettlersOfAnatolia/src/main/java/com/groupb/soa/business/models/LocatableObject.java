3e485cfc3bf93abd02ca7ad50ce8a0a44826308b
goksuturann <goksu.turan@ug.bilkent.edu.tr>
2019-11-18 21:32:05 +0200

    Player PlayerList DomasticTrade LocatableObject
    These classes added.

---
+++ src/SettlersOfAnatolia/src/main/java/com/groupb/soa/business/models/LocatableObject.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.groupb.soa.business.models;
+
+/**
+ *
+ * @author goksuturan
+ */
+public class LocatableObject {
+    double x;
+    double y;
+
+    LocatableObject( double x, double y){
+        x = this.x;
+        y = this.y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+}
