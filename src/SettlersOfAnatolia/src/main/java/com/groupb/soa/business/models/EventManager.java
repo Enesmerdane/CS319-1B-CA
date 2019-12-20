10d4b418186ca046d04d47d7124780ed5afaf497
iremkirmacii <irem.kirmaci@ug.bilkent.edu.tr>
2019-12-20 22:26:01 +0200

    EventManager is added

---
+++ src/SettlersOfAnatolia/src/main/java/com/groupb/soa/business/models/EventManager.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.groupb.soa.business.models;
+
+/**
+ *
+ * @author İrem Kırmacı
+ */
+public class EventManager {
+     Event event;
+    GameModel model;
+    public  EventManager(GameModel model){
+        this.model = model;
+    }
+
+    public Event checkEvent(){
+
+        //creating a random value between 0 and 100
+        int random = (int)(Math.random()* 100 );
+        if(random >= 0 && random <= 10){
+            if(random <= 2){
+                //event = new EarthQuake(model);
+            }
+            if(random >= 3 && random <= 5){
+                event = new Flood(model);
+            }
+            if(random >= 6 && random <= 8){
+                //event = new WolfAttack(model);
+            }
+            if(random >= 9){
+                event = new CybeleMonth(model);
+            }
+            return event;
+
+        }
+        return null;
+
+    }
+    public void handleEvent(Event event){
+
+        //if in the checkEvent, an event was occurred
+        if(event != null)
+            event.occur(model);
+    }
+}
